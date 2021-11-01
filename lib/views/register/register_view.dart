cf032f17da2f77b7b803242ea44ae188a493d355
FlutLab.io <>
2021-11-01 15:34:50 +0000

    Snapshot 'commit_006'

--- lib/views/register/register_view.dart
+++ lib/views/register/register_view.dart
@@ -1,6 +1,9 @@
+
 import 'package:flutter/material.dart';
+
+import 'package:flutter_svg/svg.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:multi_wizard/multi_wizard.dart';
+import 'package:cool_stepper/cool_stepper.dart';
 import 'package:helloworld/views/auth/sign_in.dart';
 
 class RegisterView extends StatelessWidget {
@@ -8,31 +11,30 @@ class RegisterView extends StatelessWidget {
     return MaterialApp(
       theme: ThemeData(
         colorScheme: ColorScheme.fromSwatch(
-          primarySwatch: Colors.blue,
-        ).copyWith(
-          secondary: Colors.green,
-        ),
+      primarySwatch: Colors.blue,
+    ).copyWith(
+      secondary: Colors.green,
+    ),
         visualDensity: VisualDensity.adaptivePlatformDensity,
         fontFamily: GoogleFonts.poppins().fontFamily,
       ),
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-          appBar: AppBar(
-            backgroundColor: Colors.transparent,
-            elevation: 0,
-            leading: Builder(
-              builder: (BuildContext context) {
-                return IconButton(
-                  icon: const Icon(Icons.chevron_left, color: Color(0xFF000000)),
-                  onPressed: () {
-                    Scaffold.of(context).openDrawer();
-                  },
-                  tooltip: MaterialLocalizations.of(context).openAppDrawerTooltip,
-                );
-              },
-            ),
-          ),
-          body: SignUpForm()),
+      home: Scaffold( 
+          appBar : AppBar( 
+              backgroundColor : Colors.transparent, 
+              elevation:0 , 
+              leading: Builder(
+    builder: (BuildContext context) {
+      return IconButton(
+        icon: const Icon(Icons.chevron_left,color:Color(0xFF000000)),
+        onPressed: () { Scaffold.of(context).openDrawer(); },
+        tooltip: MaterialLocalizations.of(context).openAppDrawerTooltip,
+      );
+    },
+  ),
+          ), 
+          body: SignUpForm() 
+      ),
     );
   }
 }
@@ -46,204 +48,172 @@ class SignUpForm extends StatefulWidget {
 
 class _SignUpFormState extends State<SignUpForm> {
   final _formKey = GlobalKey<FormState>();
-
-  String? _nameCtrl;
-  String? _emailCtrl;
+  String? selectedRole = 'Writer';
+  final TextEditingController _nameCtrl = TextEditingController();
+  final TextEditingController _emailCtrl = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
-    return LayoutBuilder(
-      builder: (context, constraint) {
-        return SingleChildScrollView(
-          physics: ScrollPhysics(),
-          reverse: false,
-          primary: true,
-          keyboardDismissBehavior: ScrollViewKeyboardDismissBehavior.manual,
-          child: ConstrainedBox(
-            constraints: BoxConstraints(
-              minHeight: constraint.maxHeight,
-            ),
-            child: IntrinsicHeight(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Container(
-                    alignment: Alignment.bottomCenter,
-                    width: MediaQuery.of(context).size.width,
-                    height: 200,
-                    child: Image(image: AssetImage('assets/onboarding/handing-out-offer-letter.png')),
-                  ),
-                  Container(
-                    padding: EdgeInsets.only(left: 24.0, right: 24.0, top: 30.0, bottom: 0),
-                    width: MediaQuery.of(context).size.width,
-                    child: Text(
-                      "Sign Up",
-                      style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30),
-                    ),
-                    alignment: Alignment.centerLeft,
-                  ),
-                  SizedBox(
-                    width: MediaQuery.of(context).size.width,
-                    height: MediaQuery.of(context).size.height - 380,
-                    child: MultiWizard(
-                      steps: [
-                        WizardStep(
-                          showPrevious: false, // Removes the previous button
-                          nextFunction: () => print('You pressed the next button'),
-                          child: Container(
-                            height: MediaQuery.of(context).size.height,
-                            child: Wrap(
-                              direction: Axis.vertical,
-                              alignment: WrapAlignment.start,
-                              crossAxisAlignment: WrapCrossAlignment.center,
-                              children: [
-                                Container(
-                                  padding: EdgeInsets.only(left: 24.0, right: 24.0, top: 5.0, bottom: 15.0),
-                                  width: MediaQuery.of(context).size.width,
-                                  child: Column(
-                                    mainAxisAlignment: MainAxisAlignment.start,
-                                    crossAxisAlignment: CrossAxisAlignment.start,
-                                    children: [
-                                      Container(
-                                          child: Row(mainAxisAlignment: MainAxisAlignment.start, crossAxisAlignment: CrossAxisAlignment.start, children: [
-                                            Text("Please enter your", style: TextStyle(fontSize: 12, color: Colors.grey[600])),
-                                            SizedBox(
-                                              width: 2,
-                                            ),
-                                            Text(
-                                              "Email.",
-                                              style: TextStyle(fontSize: 12, color: Colors.grey[600], fontWeight: FontWeight.bold),
-                                            )
-                                          ]),
-                                          alignment: Alignment.centerLeft),
-                                      SizedBox(height: 30),
-                                      Center(
-                                        child: Form(
-                                          key: _formKey,
-                                          child: TextFormField(
-                                            autovalidateMode: AutovalidateMode.always,
-                                            decoration: InputDecoration(hintText: 'Your Email'),
-                                            validator: (value) {
-                                              if (value!.isEmpty) {
-                                                return 'You must have a Email!';
-                                              } else if (value.length < 8) {
-                                                return 'Your Email must be atleast 8 charachters long!';
-                                              }
-                                            },
-                                            onSaved: (value) {
-                                              _emailCtrl = value!;
-                                            },
-                                          ),
-                                        ),
-                                      ),
-                                    ],
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ),
-                        ),
-                        WizardStep(
-                          showPrevious: true, // Removes the previous button
-                          nextFunction: () => print('You pressed the next button'),
-                          child: Container(
-                            height: double.infinity,
-                            child: Wrap(
-                              direction: Axis.vertical,
-                              alignment: WrapAlignment.start,
-                              crossAxisAlignment: WrapCrossAlignment.center,
-                              children: [
-                                Container(
-                                  padding: EdgeInsets.only(left: 24.0, right: 24.0, top: 5.0, bottom: 15.0),
-                                  width: MediaQuery.of(context).size.width,
-                                  child: Column(
-                                    mainAxisAlignment: MainAxisAlignment.start,
-                                    crossAxisAlignment: CrossAxisAlignment.start,
-                                    children: [
-                                      Container(
-                                        child: Row(
-                                          mainAxisAlignment: MainAxisAlignment.start,
-                                          crossAxisAlignment: CrossAxisAlignment.start,
-                                          children: [
-                                            Text("Please enter your", style: TextStyle(fontSize: 12, color: Colors.grey[600])),
-                                            SizedBox(
-                                              width: 2,
-                                            ),
-                                            Text(
-                                              "Email.",
-                                              style: TextStyle(fontSize: 12, color: Colors.grey[600], fontWeight: FontWeight.bold),
-                                            )
-                                          ],
-                                        ),
-                                        alignment: Alignment.centerLeft,
-                                      ),
-                                      SizedBox(height: 30),
-                                      Center(
-                                        child: Form(
-                                          key: _formKey,
-                                          child: TextFormField(
-                                            autovalidateMode: AutovalidateMode.always,
-                                            decoration: InputDecoration(hintText: 'Your Email'),
-                                            validator: (value) {
-                                              if (value!.isEmpty) {
-                                                return 'You must have a Email!';
-                                              } else if (value.length < 8) {
-                                                return 'Your Email must be atleast 8 charachters long!';
-                                              }
-                                            },
-                                            onSaved: (value) {
-                                              _emailCtrl = value!;
-                                            },
-                                          ),
-                                        ),
-                                      ),
-                                    ],
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ),
-                        ),
-                        WizardStep(
-                          child: Center(
-                            child: Form(
-                              key: _formKey,
-                              child: TextFormField(
-                                autovalidateMode: AutovalidateMode.always,
-                                decoration: InputDecoration(hintText: 'Your name'),
-                                validator: (value) {
-                                  if (value!.isEmpty) {
-                                    return 'You must have a name!';
-                                  } else if (value.length < 2) {
-                                    return 'Your name must be atleast 2 charachters long!';
-                                  }
-                                },
-                                onSaved: (value) {
-                                  _nameCtrl = value!;
-                                },
-                              ),
-                            ),
-                          ),
-                        ),
-                      ],
-                      finishFunction: () {
-                        if (_formKey.currentState!.validate()) {
-                          _formKey.currentState!.save();
-                          ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-                            content: Text('Welcome $_nameCtrl!'),
-                            duration: Duration(seconds: 5),
-                          ));
-                        }
-                      },
-                    ),
-                  ),
-                ],
+      final steps = [
+      CoolStep(
+        title: 'Basic Information',
+        subtitle: 'Please fill some of the basic information to get started',
+        content: Form(
+          key: _formKey,
+          child: Column(
+            children: [
+              _buildTextField(
+                labelText: 'Name',
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return 'Name is required';
+                  }
+                  return null;
+                },
+                controller: _nameCtrl,
               ),
-            ),
+              _buildTextField(
+                labelText: 'Email Address',
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return 'Email address is required';
+                  }
+                  return null;
+                },
+                controller: _emailCtrl,
+              ),
+            ],
           ),
-        );
+        ),
+        validation: () {
+          if (!_formKey.currentState!.validate()) {
+            return 'Fill form correctly';
+          }
+          return null;
+        },
+      ),
+      CoolStep(
+        title: 'Select your role',
+        subtitle: 'Choose a role that better defines you',
+        content: Container(
+          child: Row(
+            children: <Widget>[
+              _buildSelector(
+                context: context,
+                name: 'Writer',
+              ),
+              SizedBox(width: 5.0),
+              _buildSelector(
+                context: context,
+                name: 'Editor',
+              ),
+            ],
+          ),
+        ),
+        validation: () {
+          return null;
+        },
+      ),
+    ];
+
+    final stepper = CoolStepper(
+      showErrorSnackbar: false,
+      onCompleted: () {
+        print('Steps completed!');
       },
+      steps: steps,
+      config: CoolStepperConfig(
+        backText: 'PREV',
+      ),
+    );
+    
+      return LayoutBuilder(builder: (context, constraint) {
+      return SingleChildScrollView(
+        physics: ScrollPhysics(),
+        reverse: false,
+        primary:true, 
+        keyboardDismissBehavior: ScrollViewKeyboardDismissBehavior.manual,
+        child: ConstrainedBox(
+          constraints: BoxConstraints(
+            minHeight: constraint.maxHeight,
+          ),
+          child: IntrinsicHeight( 
+              child : Column ( 
+                  children : [ 
+                      Container(
+                  alignment: Alignment.bottomCenter,
+                  width: MediaQuery.of(context).size.width,
+                  height: 200,
+                  child: Image(
+                      image: AssetImage( 'assets/onboarding/handing-out-offer-letter.png')),
+                      ),
+                      Container( 
+                          child : stepper
+                      ) , 
+                      Container( 
+                          child : Text("Column 3") 
+                      ) , 
+                  ]
+              ) 
+          ) 
+        ), 
+      ) ;
+      });
+   
+          
+    }
+    Widget _buildTextField({
+    String? labelText,
+    FormFieldValidator<String>? validator,
+    TextEditingController? controller,
+  }) {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 20.0),
+      child: TextFormField(
+        decoration: InputDecoration(
+          labelText: labelText,
+        ),
+        validator: validator,
+        controller: controller,
+      ),
     );
   }
-}
+
+  Widget _buildSelector({
+    BuildContext? context,
+    required String name,
+  }) {
+    final isActive = name == selectedRole;
+
+    return Expanded(
+      child: AnimatedContainer(
+        duration: Duration(milliseconds: 200),
+        curve: Curves.easeInOut,
+        decoration: BoxDecoration(
+          color: isActive ? Theme.of(context!).primaryColor : null,
+          border: Border.all(
+            width: 0,
+          ),
+          borderRadius: BorderRadius.circular(8.0),
+        ),
+        child: RadioListTile(
+          value: name,
+          activeColor: Colors.white,
+          groupValue: selectedRole,
+          onChanged: (String? v) {
+            setState(() {
+              selectedRole = v;
+            });
+          },
+          title: Text(
+            name,
+            style: TextStyle(
+              color: isActive ? Colors.white : null,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+    
+  }
