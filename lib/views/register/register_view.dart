173d8a080fee50de97854d61f0018a27923f374d
FlutLab.io <>
2021-11-02 15:14:00 +0000

    Autosave

--- lib/views/register/register_view.dart
+++ lib/views/register/register_view.dart
@@ -191,7 +191,7 @@ class _SignUpFormState extends State<SignUpForm> {
                                     Text("Already account? Please "), 
                                         GestureDetector ( 
                                         onTap:() {}, 
-                                        Text("Sign In",style: TextStyle(fontWeight: FontWeight.bold,),), 
+child :                                       Text("Sign In",style: TextStyle(fontWeight: FontWeight.bold,),), 
                                         ) , 
                                     ]
                                 ), 
