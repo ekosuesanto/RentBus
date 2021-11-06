15b5257cff33c616f578ef99b62a10b48b7e5f60
FlutLab.io <>
2021-11-06 07:31:40 +0000

    Autosave

--- lib/views/main.dart
+++ lib/views/main.dart
@@ -3,7 +3,15 @@ import 'package:shared_preferences/shared_preferences.dart';
 import 'package:helloworld/views/auth/sign_in.dart';
 class ViewMain extends StatefulWidget
 {
-    const ViewMain({Key? key}) : super(key: key);
-    @override
+  const ViewMain({Key? key}) : super(key: key);
+  @override
   _ViewMainState createState() => _ViewMainState();
+}
+
+class _ViewMainState extends State<ViewMain> { 
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(child: Text("Home"),), 
+    );
 }
