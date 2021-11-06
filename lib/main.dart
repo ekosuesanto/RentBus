a158ac2edce1173dfef4444ce9935ff3c9756cf3
FlutLab.io <>
2021-11-06 06:39:48 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'dart:async';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:helloworld/onboarding.dart';
