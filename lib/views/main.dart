29a1cdc441a373fd473796d911d098f84dd03f1e
FlutLab.io <>
2021-11-07 11:52:59 +0000

    Autosave

--- lib/views/main.dart
+++ lib/views/main.dart
@@ -14,7 +14,7 @@ class _ViewMainState extends State<ViewMain> {
     
   Widget _buildCustomProfil (String fullname, String logo, double logoSize,bool showNotificationIcon) {
     return Container( 
-      padding : EdgeInsets.only(top: 15,bottom:15,right: 24,left: 24) , 
+      padding : EdgeInsets.only(top: 50,bottom:15,right: 24,left: 24) , 
       child: Row ( 
         mainAxisAlignment : MainAxisAlignment.spaceBetween, 
         children : [ 
