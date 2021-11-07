1884ec2616db7faf22ec33d52ef8e17ae4b3308c
FlutLab.io <>
2021-11-07 14:53:23 +0000

    Autosave

--- lib/views/main.dart
+++ lib/views/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:banner_carousel/banner_carousel.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:helloworld/views/auth/sign_in.dart';
