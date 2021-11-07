60819616c45e2a0a25f066d201b9f5520aeb31cf
FlutLab.io <>
2021-11-07 08:29:02 +0000

    Autosave

--- lib/views/main.dart
+++ lib/views/main.dart
@@ -12,7 +12,7 @@ class ViewMain extends StatefulWidget
 class _ViewMainState extends State<ViewMain> { 
   
     
-  Widget _buildCustomProfil (String fullname, AssetImage logo, {bool showNotificationIcon = false}) {
+  Widget _buildCustomProfil (String fullname, String logo, {bool showNotificationIcon = false}) {
     return Container( 
       child: Row ( 
         mainAxisAlignment : MainAxisAlignment.spaceBetween, 
