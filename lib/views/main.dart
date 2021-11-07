928d5987cea205ca9ebea022a0cc016f8558bc7a
FlutLab.io <>
2021-11-07 14:16:09 +0000

    Autosave

--- lib/views/main.dart
+++ lib/views/main.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:helloworld/views/auth/sign_in.dart';
+import 'package:dot_navigation_bar/dot_navigation_bar.dart';
+
 class ViewMain extends StatefulWidget
 {
   const ViewMain({Key? key}) : super(key: key);
@@ -11,7 +13,15 @@ class ViewMain extends StatefulWidget
 
 class _ViewMainState extends State<ViewMain> { 
   bool showNotificationIcon = true;
-    
+  var _selectedTab = _SelectedTab.home;
+
+  void _handleIndexChanged(int i) {
+    setState(() {
+      _selectedTab = _SelectedTab.values[i];
+    });
+  }
+  
+  
   Widget _buildCustomProfil (String fullname, AssetImage logo, double logoSize,bool showNotificationIcon) {
     return Container( 
       padding : EdgeInsets.only(top: 50,bottom:15,right: 24,left: 24) , 
@@ -76,7 +86,7 @@ class _ViewMainState extends State<ViewMain> {
               crossAxisAlignment : CrossAxisAlignment.start, 
               children : [ 
                 Container( 
-                    child:_buildCustomProfil("Eko Susanto",AssetImage("assets/icon/default-user-icon.png"),50.0,true),
+                    child: _buildCustomProfil("Eko Susanto",AssetImage("assets/icon/default-user-icon.png"),50.0,true),
                 ),
                 Container( 
                     child:Text("Container 2"),
@@ -89,6 +99,37 @@ class _ViewMainState extends State<ViewMain> {
           ),
         ), 
       ),
+      bottomNavigationBar:  DotNavigationBar(
+          currentIndex: _SelectedTab.values.indexOf(_selectedTab),
+          onTap: _handleIndexChanged,
+          // dotIndicatorColor: Colors.black,
+          items: [
+            /// Home
+            DotNavigationBarItem(
+              icon: Icon(Icons.home),
+              selectedColor: Colors.purple,
+            ),
+
+            /// Likes
+            DotNavigationBarItem(
+              icon: Icon(Icons.favorite_border),
+              selectedColor: Colors.pink,
+            ),
+
+            /// Search
+            DotNavigationBarItem(
+              icon: Icon(Icons.search),
+              selectedColor: Colors.orange,
+            ),
+
+            /// Profile
+            DotNavigationBarItem(
+              icon: Icon(Icons.person),
+              selectedColor: Colors.teal,
+            ),
+            
+          ],
+        ),
     );
   }
 }
