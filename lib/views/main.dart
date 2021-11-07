199d60039399be393bea21b243af6ed50d3fa33e
FlutLab.io <>
2021-11-07 16:40:51 +0000

    Autosave

--- lib/views/main.dart
+++ lib/views/main.dart
@@ -123,7 +123,7 @@ class _ViewMainState extends State<ViewMain> {
                                   ),
                                   VerticalDivider(),
                                   Container( 
-                                    padding : EdgeInsets.only(top: 20,bottom:20,right: 20,left: 20) , 
+                                    padding : EdgeInsets.only(top: 20,bottom:20,right: 0,left: 0) , 
                                     width : MediaQuery.of(context).size.width / 3,
                                     child : Row( children : [ 
                                     Container( 
