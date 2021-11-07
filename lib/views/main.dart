a73cb94cc0b1e2c4de7cec2a77b754078817608a
FlutLab.io <>
2021-11-07 16:50:36 +0000

    Autosave

--- lib/views/main.dart
+++ lib/views/main.dart
@@ -120,7 +120,7 @@ class _ViewMainState extends State<ViewMain> {
                                   
                                   Container( 
                                     padding : EdgeInsets.only(top: 20,bottom:20,) , 
-                                    width : MediaQuery.of(context).size.width / 3,
+                                    width : MediaQuery.of(context).size.width / 4,
                                     child : Row( children : [ 
                                     Container( 
 
