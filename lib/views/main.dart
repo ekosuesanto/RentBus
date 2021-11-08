08c667f4d0e90a7d502b9276f8e2c8484dd1b7af
FlutLab.io <>
2021-11-08 18:28:51 +0000

    Autosave

--- lib/views/main.dart
+++ lib/views/main.dart
@@ -741,7 +741,7 @@ class _ViewMainState extends State<ViewMain> {
                     left: 24,
                   ),
                   width: MediaQuery.of(context).size.width,
-                  height: 150,
+                  height: 200,
                   child: ListView.builder(
                     
                     scrollDirection : Axis.horizontal, 
