0f8fd7869c7fc4235f39238f2fe2b12a0bd1a7a3
FlutLab.io <>
2021-11-09 19:02:57 +0000

    Autosave

--- lib/views/main.dart
+++ lib/views/main.dart
@@ -759,7 +759,7 @@ class _ViewMainState extends State<ViewMain> {
  BoxShadow(
   color: Colors.black.withOpacity(0.05),
   spreadRadius: 3,
-  blurRadius: 20, 
+  blurRadius: 15, 
     offset: Offset(-3, 3), 
  )
 ],
