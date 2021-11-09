b44f06ba846c4779597a897660e7cddebfa99b60
FlutLab.io <>
2021-11-09 20:24:33 +0000

    Autosave

--- lib/views/main.dart
+++ lib/views/main.dart
@@ -783,7 +783,7 @@ class _ViewMainState extends State<ViewMain> {
   clipBehavior: Clip.hardEdge,
   children: <Widget>[
     Positioned(
-      top: 72,
+      top: 75,
       left: -15,
       height:30,
       width: 30,
@@ -815,7 +815,7 @@ class _ViewMainState extends State<ViewMain> {
   clipBehavior: Clip.hardEdge,
   children: <Widget>[
     Positioned(
-      top: 72,
+      top: 75,
       left: MediaQuery. of(context).size.width - 63,
       height:30,
       width: 30,
